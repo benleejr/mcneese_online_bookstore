e1d6f398f1c6063fc612be2aaefb36843032709b
BrennanJanuary <112736420+BrennanJanuary@users.noreply.github.com>
2023-09-28 12:55:43 -0500

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,27 +4,33 @@ const footerSections = [
   {
     className: 'contact-info',
     content: 'Contact information',
+    link: '/contact', // Add the URL for the Contact section
   },
   {
     className: 'about',
     content: 'About section',
+    link: '/about', // Add the URL for the About section
   },
   {
     className: 'social-media',
     content: 'Social media links',
+    link: '/social-media', // Add the URL for the Social Media section
   },
   {
     className: 'careers',
-    content: 'Careers',
+    content: 'Careers section',
+    link: '/careers', // Add the URL for the Careers section
   },
 ];
 
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
+     
+
       {footerSections.map((section) => (
         <div key={section.className} className={section.className}>
-          {section.content}
+          <a href={section.link}>{section.content}</a>
         </div>
       ))}
 
@@ -52,5 +58,13 @@ const Footer: React.FC = () => {
     </footer>
   );
 };
+
+
+
+
+
+
+
+
 
 export default Footer;
